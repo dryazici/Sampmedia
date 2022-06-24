ad76e59726bec79f1107a11a28732436c8746223
dryazici <107036709+dryazici@users.noreply.github.com>
2022-06-25 00:03:00 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,144 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ disableMentions: 'everyone' });
+const ayarlar = require('./ayarlar.json');
+const fs = require('fs');
+const moment = require('moment');
+require('./util/eventLoader')(client);
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.on('ready', () => {
+
+  // Oynuyor Kısmı
+  
+      var actvs = [
+        `${prefix}yardım ${client.guilds.cache.size} sunucuyu`,
+        `${prefix}yardım ${client.users.cache.size} Kullanıcıyı`, 
+        `${prefix}yardım`
+    ];
+    
+    client.user.setActivity(actvs[Math.floor(Math.random() * (actvs.length - 1) + 1)], { type: 'LISTENING' });
+    setInterval(() => {
+        client.user.setActivity(actvs[Math.floor(Math.random() * (actvs.length - 1) + 1)], { type: 'LISTENING'});
+    }, 15000);
+    
+  
+      console.log ('_________________________________________');
+      console.log (`Kullanıcı İsmi     : ${client.user.username}`);
+      console.log (`Sunucular          : ${client.guilds.cache.size}`);
+      console.log (`Kullanıcılar       : ${client.users.cache.size}`);
+      console.log (`Prefix             : ${ayarlar.prefix}`);
+      console.log (`Durum              : Bot Çevrimiçi!`);
+      console.log ('_________________________________________');
+    
+    });
+
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+var sqarlex = [
+`Dr Yazıcı 😎`, //Değiştirlecek
+`Dr Yazıcı 👑`, //Değiştirlecek
+`Dr Yazıcı 🤠`, //Değiştirlecek
+`Dr Yazıcı💪🏿`, //Değiştirlecek
+`Dr Yazıcı👍🏿` //Değiştirlecek
+]
+client.on("ready", () => {
+setInterval(function() {
+var random = Math.floor(Math.random()*(sqarlex.length-0+1)+0);
+client.user.setActivity(sqarlex[random], {"type": "PLAYING"});
+//WATCHING = İZLİYOR
+//PLAYING = OYNUYOR
+//LISTENING = DİNLİYOR
+//STREAMING =YAYINDA
+}, 2 * 3000);
+});
+
+client.on("guildCreate", async guild => {
+  const selam = [
+    "Beni sunucuna eklediğin için teşekkür ederim.",
+    "**Benim prefixim (.) dir.**",
+    '**Beni eklediğin için çok mutluyum 😄**'
+  ];
+  guild.owner.send(selam);
+  console.log(`LOG: ${guild.name}. sunucuya katıldım!`);
+});
+
+
+client.login(ayarlar.token);
